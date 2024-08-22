4b1861a3c1e699706b6cdda590257773295f1140
kosei <ohara.kosei.s3@dc.tohoku.ac.jp>
2024-08-22 21:33:00 +0900

    NEW : exec.sh was made
    Shell script file is made to execute the program with qsub command.

---
+++ src/exec.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#PBS -q tqueue
+#PBS -N UNDEF_REPLACE
+#PBS -j oe
+#PBS -l nodes=1:ppn=1
+
+VAR="TTSWR"
+NOW=$(date "+%Y%m%d_%H%M%S")
+RESULT="../output/result_${VAR}_${NOW}.txt"
+
+NAMELIST="../nml/input_${VAR}.nml"
+
+ulimit -s unlimited
+
+cd /mnt/jet11/kosei/JRA/undef_filter/src
+
+./EXE < ${NAMELIST} > ${RESULT} 2>&1
+
